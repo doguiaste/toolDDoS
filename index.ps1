63d237d680441efbf5d438b083472e2dbd4b7356
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-07-07 08:39:11 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,7 +1,9 @@
-$pythonPath = "python"  # Sistem PATH'te python varsa böyle bırak, yoksa python.exe'nin tam yolunu yaz
+$python = "python"  # Python PATH’te yoksa tam yolunu yaz
 $url = "https://raw.githubusercontent.com/doguiaste/toolDDoS/main/start.py"
-$tempFile = "$env:TEMP\start.py"
 
-Invoke-WebRequest -Uri $url -OutFile $tempFile
-& $pythonPath $tempFile
-Remove-Item $tempFile
+# Geçici dosya yolu
+$tmp = "$env:TEMP\start.py"
+
+Invoke-RestMethod -Uri $url -OutFile $tmp
+& $python $tmp
+Remove-Item $tmp
